c450ddf480a4bb460f694e513f9148ca12078edf
DESKTOP-HU04ENR\estrategas <brandon10torres@gmail.com>
2017-10-06 13:22:20 -0500

    Proyecto3DCardboard interacción con la vista

---
+++ Proyecto3DCardboard/Assets/Scripts/UIControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour
+{
+
+    public SkinnedMeshRenderer mallaMounstruo;
+    public MeshRenderer mallaCubo;
+
+    public Color colorHoverMounstruo;
+    public Color colorHoverCubo;
+
+    private Color colorInicialMounstruo;
+    private Color colorInicialCubo;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        colorInicialMounstruo = mallaMounstruo.material.color;
+        colorInicialCubo = mallaCubo.material.color;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void HoverMounstruo()
+    {
+        mallaMounstruo.material.color = colorHoverMounstruo;
+    }
+
+    public void HoverExitMounstruo()
+    {
+        mallaMounstruo.material.color = colorInicialMounstruo;
+    }
+
+    public void HoverCubo()
+    {
+        mallaCubo.material.color = colorHoverCubo;
+    }
+
+    public void HoverExitCubo()
+    {
+        mallaCubo.material.color = colorInicialCubo;
+    }
+}
