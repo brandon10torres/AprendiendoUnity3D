845787d8d24b40937abb8050518b9bb1f78ee6b5
DESKTOP-HU04ENR\estrategas <brandon10torres@gmail.com>
2017-10-06 12:21:47 -0500

    Proyecto3D inicio adaptación Cardboard

---
+++ Proyecto3DCardboard/Assets/Scripts/disparoProyectil.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disparoProyectil : MonoBehaviour 
+{
+
+	public GameObject proyectil;
+	public GameObject miraProyectil;
+	public float fuerzaProyectil = 500.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			GameObject objProyectil = Instantiate (proyectil, miraProyectil.transform.position, Quaternion.identity);
+			Destroy (objProyectil, 2.0f);
+			objProyectil.GetComponent<Rigidbody> ().AddForce (miraProyectil.transform.forward * fuerzaProyectil);
+		}
+	}
+}
